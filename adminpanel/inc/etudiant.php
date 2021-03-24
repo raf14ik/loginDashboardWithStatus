97c240d48c28e3fcacf042079077e7a88044a551
rafik hammas <rafik.hammas@outlook.com>
2021-03-24 18:43:14 +0100

    Admin_ManageStudents

---
+++ adminpanel/inc/etudiant.php
@@ -0,0 +1,76 @@
+<?php
+// created by rafik hammas
+// etudiant related functions 
+
+//create student
+function CreateEtud($conn,$firstname,$secondname,$speciality,$email,$password)
+{		
+	$datetoday = date('Y-m-d'); 
+	if($firstname==NULL ||$secondname==NULL || $speciality==NULL ||$email==NULL || $password==NULL)
+	{
+		$response ='error';
+		return $response;
+	}
+	else{
+		$password=md5(sha1($password));
+		mysqli_query($conn,"INSERT INTO `etudiant` (`firstname`, `secondname`, `speciality`, `email`, `password`, `created`, `status`) VALUES
+		('$firstname','$secondname','$speciality','$email','$password','$datetoday','1')"); 
+
+			$response ='success';
+			return $response;
+	}
+}
+//get Student details
+function GetStud($conn,$eid)
+{
+    $sqlq = mysqli_query($conn,"SELECT * FROM `etudiant` WHERE `eid`=$eid");
+    $rowstud =mysqli_fetch_array($sqlq);        
+    $studs['firstname'] = $rowstud['firstname']; 
+    $studs['secondname'] = $rowstud['secondname']; 
+    $studs['speciality'] = $rowstud['speciality']; 
+    $studs['email'] = $rowstud['email']; 
+    $studs['password'] = $rowstud['password']; 
+    $studs['created'] = $rowstud['created']; 
+    $studs['status'] = $rowstud['status']; 
+    return $studs;
+}	
+
+//get students list
+function GetStudList($conn)
+{
+    $sqlq = mysqli_query($conn,"SELECT * FROM `etudiant` ");
+    $data =array();
+    while($row =mysqli_fetch_assoc($sqlq))
+    {
+        $data[] = $row; 
+    }    
+    return $data;
+}
+
+//update student detail
+function ChangeDetail($conn,$eid,$firstname,$secondname,$speciality,$email,$newpass,$rpass)
+{		
+    if($eid!=NULL)
+    {
+        if($newpass==$rpass)
+        {
+            $password=md5(sha1($newpass));
+            mysqli_query($conn,"UPDATE `etudiant` SET `firstname`='$firstname',`secondname`='$secondname',`speciality`='$speciality',`email`='$email', `password` = '$password' WHERE `etudiant`.`eid` = $eid");
+            $response ='success';
+            return $response;
+        }
+        else
+        {
+            $response ='pwmiss';
+            return $response;
+        }
+    }
+    else
+    {
+        $response ='error';
+        return $response;
+    }
+}
+
+
+?>
---
+++ adminpanel/inc/main.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$softwarename = "Perfect Web Technology";  // Application name 
+
+$softwareversion ='1.0.0';  // Application version 
+
+$softwaredescrip ="Welcome";  //  Application description 
+
+
+
+
+?>
